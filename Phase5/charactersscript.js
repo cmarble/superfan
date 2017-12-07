b84e43d159b064d2cbc2d071bdbd211084faff79
cmarble <camryn_marble@davincischools.org>
2017-12-07 14:43:05 -0800

    Add files via upload

---
+++ Phase5/charactersscript.js
@@ -0,0 +1,10 @@
+function on(elem){
+	document.getElementById("sinfo").classList.remove("display-none");
+	document.getElementById("shawn").classList.add("display-none");
+}
+
+
+//function on(elem){
+//	document.getElementById("sinfo").classList.add("display-none");
+//	document.getElementById("shawn").classList.remove("display-none");
+//}
